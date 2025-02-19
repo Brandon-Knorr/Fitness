721795243a48541df422fde02686d8609d1f05c9
Brandon-Knorr <bknorr@my.wctc.edu>
2025-02-19 12:01:13 -0600

    [1.29a] working on collecting form data

--- fitness.js
+++ fitness.js
@@ -6,3 +6,14 @@ function elementFromHTML(html) {
     return template.content.firstElementChild;
 };
 
+const form = document.querySelector('#workout');
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const fd = new FormData(form);
+    const obj = Object.fromEntries(fd);
+    console.log(obj);
+
+    const json = JSON.stringify(obj);
+    console.log(json);
+});
