3e486f9b59fc8dbd0cf8a73ab13fb194b94dccd5
Brandon-Knorr <bknorr@my.wctc.edu>
2025-03-11 18:36:59 -0500

    [5.07b] workoutListComponent structure

---
+++ js/components/WorkoutListComponent.js
@@ -0,0 +1,17 @@
+const WorkoutListComponent = {
+  data: function () {
+    return {};
+  },
+
+  props: {},
+
+  methods: {},
+
+  computed: {},
+
+  watch: {},
+
+  template: ``,
+};
+
+export default WorkoutListComponent;
