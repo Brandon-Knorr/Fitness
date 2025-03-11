3623315249fc0b800565357735da30486b80c7f5
Brandon-Knorr <bknorr@my.wctc.edu>
2025-03-11 17:03:17 -0500

    [5.05c] added props, methods, computed properties, and watch to progressbar component

--- js/components/ProgressbarComponent.js
+++ js/components/ProgressbarComponent.js
@@ -6,26 +6,73 @@ const ProgressbarComponent = {
   //data is a function that returns an object
 
   data: function () {
-    return {};
+    return {
+      user: {
+        name: "Brandon Knorr",
+        weight: 198,
+        rank: "Novice",
+      },
+    };
   },
-
   //props Data that are passed into the component
   //can be optional or required
   //objects and arrays are passed by reference
   //primitives are passed by value
-  props: {},
+  props: {
+    title: {
+      type: String,
+      default: "Progress Until Next Rank",
+    },
+    workOutsNeededForNextRank: {
+      type: Number,
+      default: 15,
+    },
+    totalWorkOuts: {
+      type: Number,
+      default: 0,
+    },
+    workouts: {
+      type: Array,
+      default: () => [],
+    },
+  },
 
   //methods are usually events triggered by v-on
-  methods: {},
+  methods: {
+    rankUp() {
+      const ranks = ["Novice", "Intermediate", "Advanced", "Elite"];
+      let currentIndex = ranks.indexOf(this.user.rank);
+      if (currentIndex < ranks.length - 1) {
+        this.user.rank = ranks[currentIndex + 1];
+      }
+    },
+  },
 
   //computed properties are values that are cached and only updated when the values they depend on change
   //computed value functions need to return a value
   //Treat these like regular values that you would use in data or props
-  computed: {},
+  computed: {
+    progress() {
+      const percentage =
+        (this.totalWorkouts / this.workoutsNeededForNextRank) * 100;
+      return Math.round(percentage);
+    },
+    totalWorkOuts() {
+      return this.workouts.length;
+    },
+  },
 
   //watch properties are used to watch for changes in data
-  watch: {},
-
+  watch: {
+    workouts: {
+      handler(newWorkouts) {
+        if (this.progress >= 100) {
+          this.rankUp();
+        }
+      },
+      deep: true,
+    },
+  },
   //Template is the html that will be rendered
   //it should consist of one root element
   //you can reference data, props, methods, computed properties, and watch properties using {{}}
