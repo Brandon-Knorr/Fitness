974472d22824427317f67432dd54c5cc1aad9521
Brandon-Knorr <bknorr@my.wctc.edu>
2025-03-12 11:06:14 -0500

    [5.10a] AddworkoutmodalComponent -created file -created structure - added data

---
+++ js/components/AddWorkoutModalComponent.js
@@ -0,0 +1,22 @@
+const AddWorkoutModalComponent = {
+  data: function () {
+    return {
+      newExercise: "",
+      newSets: "",
+      newReps: "",
+      newWeight: "",
+      newDuration: "",
+      newCategory: "",
+    };
+  },
+
+  props: {},
+
+  methods: {},
+
+  computed: {},
+
+  watch: {},
+
+  template: ``,
+};
