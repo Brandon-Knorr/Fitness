d903c6b566cd6774be71ef4328333273d956a859
Brandon-Knorr <bknorr@my.wctc.edu>
2025-03-11 23:47:26 -0500

    [5.07c] WorkoutListComponent data, props, and computed property.

--- js/components/WorkoutListComponent.js
+++ js/components/WorkoutListComponent.js
@@ -1,13 +1,52 @@
 const WorkoutListComponent = {
   data: function () {
-    return {};
+    return {
+      newExercise: "",
+      newSets: "",
+      newReps: "",
+      newWeight: "",
+      newDuration: "",
+      newUserWeight: "",
+      newCategory: "",
+      exerciseCategories: {
+        "bench-press": "Strength",
+        squats: "Strength",
+        deadlifts: "Strength",
+        "overhead-press": "Strength",
+        "barbell-rows": "Strength",
+        running: "Cardio",
+        "jumping-jacks": "Cardio",
+        cycling: "Cardio",
+        hiit: "Cardio",
+        swimming: "Cardio",
+        "yoga-flow": "Mobility",
+        "dynamic-stretching": "Mobility",
+        "foam-rolling": "Mobility",
+        "joint-mobility": "Mobility",
+        "static-stretching": "Mobility",
+        plank: "Core",
+        "russian-twists": "Core",
+        "leg-raises": "Core",
+        crunches: "Core",
+        "mountain-climbers": "Core",
+      },
+    };
   },
 
-  props: {},
+  props: {
+    workouts: {
+      type: Array,
+      default: () => [],
+    },
+  },
 
   methods: {},
 
-  computed: {},
+  computed: {
+    totalWorkouts() {
+      return this.workouts.length;
+    },
+  },
 
   watch: {},
 
