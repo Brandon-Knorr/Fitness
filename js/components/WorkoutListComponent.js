b1980a0012ab166ff36482875df22fd1814dd900
Brandon-Knorr <bknorr@my.wctc.edu>
2025-03-12 00:39:28 -0500

    [5.07e] update the workoutlist component and app.js to have the proper properties.

--- js/components/WorkoutListComponent.js
+++ js/components/WorkoutListComponent.js
@@ -40,43 +40,7 @@ const WorkoutListComponent = {
     },
   },
 
-  methods: {
-    autoSetCategory() {
-      this.newCategory = this.exerciseCategories[this.newExercise] || "";
-    },
-    addWorkout() {
-      if (this.newExercise && this.newSets && this.newReps && this.newWeight) {
-        this.workouts.push({
-          name: this.newExercise,
-          category: this.newCategory,
-          sets: this.newSets,
-          reps: this.newReps,
-          weight: this.newWeight,
-          duration: this.newDuration || null,
-        });
-
-        this.newExercise = "";
-        this.newSets = "";
-        this.newReps = "";
-        this.newWeight = "";
-        this.newDuration = "";
-        this.newCategory = "";
-
-        this.$nextTick(() => {
-          const workoutModal = bootstrap.Modal.getInstance(
-            document.getElementById("staticBackdrop")
-          );
-          if (workoutModal) {
-            workoutModal.hide();
-          }
-          const workoutsTab = document.querySelector("#pills-workouts-tab");
-          if (workoutsTab) {
-            workoutsTab.click();
-          }
-        });
-      }
-    },
-  },
+  methods: {},
 
   computed: {
     totalWorkouts() {
