9224fddf43f4f7a00e1b9761e82da9a0dc4dc327
Brandon-Knorr <bknorr@my.wctc.edu>
2025-04-09 00:13:02 -0500

    [12.02] #1 - fixing code smells. creating constants -rank

---
+++ vue-fitness-sfc/src/constants/ranks.js
@@ -0,0 +1,8 @@
+export const RANKS = {
+  NOVICE: "Novice",
+  INTERMEDIATE: "Intermediate",
+  ADVANCED: "Advanced",
+  ELITE: "Elite",
+};
+
+export const RANK_ORDER = ["Novice", "Intermediate", "Advanced", "Elite"];
